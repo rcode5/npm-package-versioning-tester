92c47918d52aa9ee30c1571781a16f7a17fac117
Jon Rogers <jon@carbonfive.com>
2019-08-22 11:28:53 -0700

    make changes to package

--- packages/package-version-tester/index.js
+++ packages/package-version-tester/index.js
@@ -7,13 +7,4 @@ function name() {
   return package.name;
 }
 
-function noop() {
-}
-
-function noop2() {
-}
-
-function no_op3() {
-}
-
-module.exports = { version, name, noop, noop2, no_op3 }
+module.exports = { version, name }
